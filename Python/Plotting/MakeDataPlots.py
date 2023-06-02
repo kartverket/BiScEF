0207d461cfd66c20c75caa5193996a9021893142
jacokn <jacokn@kartverket.no>
2023-06-02 11:02:15 +0200

    Plotting script: Converted all plotting code to functions. Expanded options. Added checks for existence of data.

--- Python/Plotting/MakeDataPlots.py
+++ Python/Plotting/MakeDataPlots.py
@@ -15,35 +15,37 @@ def plot_timeseries_simple(constFilter, constStr):
 
 	if 'Phi60s1' in f.keys():
 		hasValue = f['Phi60s1'][()] > 0
-		if 'Unit' in f['Phi60s1'].attrs.keys():
-			unitStr = f['Phi60s1'].attrs['Unit']
-		else:
-			unitStr = '?'
-		axs[0].plot(list(compress(mdates.date2num(dt_utcdatetimes), constFilter & hasValue)), list(compress(f['Phi60s1'][()], constFilter & hasValue)), '.')
-		axs[0].xaxis.set_major_formatter(mdates.DateFormatter('%H'))
-		axs[0].xaxis.set_major_locator(mdates.HourLocator())
-		axs[0].set_xlim([min(mdates.date2num(dt_utcdatetimes)), max(mdates.date2num(dt_utcdatetimes))])
-		axs[0].set_ylim([0, 1])
-		axs[0].set_ylabel(r'Sig1 $\sigma_\phi$ (' + unitStr + ')')
-		axs[0].set_title('Phase Scintillation, ' + recCode + ', ' + constStr + ', ' + dateStr)
-		axs[0].grid()
-		plotWasMade = True
+		if sum(constFilter & hasValue) > 0:
+			if 'Unit' in f['Phi60s1'].attrs.keys():
+				unitStr = f['Phi60s1'].attrs['Unit']
+			else:
+				unitStr = '?'
+			axs[0].plot(list(compress(mdates.date2num(dt_utcdatetimes), constFilter & hasValue)), list(compress(f['Phi60s1'][()], constFilter & hasValue)), '.')
+			axs[0].xaxis.set_major_formatter(mdates.DateFormatter('%H'))
+			axs[0].xaxis.set_major_locator(mdates.HourLocator())
+			axs[0].set_xlim([min(mdates.date2num(dt_utcdatetimes)), max(mdates.date2num(dt_utcdatetimes))])
+			axs[0].set_ylim([0, 1])
+			axs[0].set_ylabel(r'Sig1 $\sigma_\phi$ (' + unitStr + ')')
+			axs[0].set_title('Phase Scintillation, ' + recCode + ', ' + constStr + ', ' + dateStr)
+			axs[0].grid()
+			plotWasMade = True
 
 	if 'Phi60s2' in f.keys():
 		hasValue = f['Phi60s2'][()] > 0
-		if 'Unit' in f['Phi60s2'].attrs.keys():
-			unitStr = f['Phi60s2'].attrs['Unit']
-		else:
-			unitStr = '?'
-		axs[1].plot(list(compress(mdates.date2num(dt_utcdatetimes), constFilter & hasValue)), list(compress(f['Phi60s2'][()], constFilter & hasValue)), '.')
-		axs[1].xaxis.set_major_formatter(mdates.DateFormatter('%H'))
-		axs[1].xaxis.set_major_locator(mdates.HourLocator())
-		axs[1].set_xlim([min(mdates.date2num(dt_utcdatetimes)), max(mdates.date2num(dt_utcdatetimes))])
-		axs[1].set_ylim([0, 1])
-		axs[1].set_xlabel('UTC hour-of-day')
-		axs[1].set_ylabel(r'Sig2 $\sigma_\phi$ (' + unitStr + ')')
-		axs[1].grid()
-		plotWasMade = True
+		if sum(constFilter & hasValue) > 0:
+			if 'Unit' in f['Phi60s2'].attrs.keys():
+				unitStr = f['Phi60s2'].attrs['Unit']
+			else:
+				unitStr = '?'
+			axs[1].plot(list(compress(mdates.date2num(dt_utcdatetimes), constFilter & hasValue)), list(compress(f['Phi60s2'][()], constFilter & hasValue)), '.')
+			axs[1].xaxis.set_major_formatter(mdates.DateFormatter('%H'))
+			axs[1].xaxis.set_major_locator(mdates.HourLocator())
+			axs[1].set_xlim([min(mdates.date2num(dt_utcdatetimes)), max(mdates.date2num(dt_utcdatetimes))])
+			axs[1].set_ylim([0, 1])
+			axs[1].set_xlabel('UTC hour-of-day')
+			axs[1].set_ylabel(r'Sig2 $\sigma_\phi$ (' + unitStr + ')')
+			axs[1].grid()
+			plotWasMade = True
 
 	if plotWasMade:
 		plotfname = datapath + '/' + fnamestem + '_PhaseScint' + '_' + constStr + '.png'
@@ -51,6 +53,85 @@ def plot_timeseries_simple(constFilter, constStr):
 		print("Saved plot to " + plotfname)
 
 
+def plot_timeseries_box(constFilter, constStr):
+	hours = range(0, 24)
+	fig, axs = plt.subplots(2)
+	plotWasMade = False
+	if 'Phi60s1' in f.keys():
+		hasValue = f['Phi60s1'][()] > 0
+		if sum(constFilter & hasValue) > 0:
+			dHourlyPhi60s1 = []
+			for hour in hours:
+				tr = [x.hour == hour for x in dt_utcdatetimes]
+				dHourlyPhi60s1.append(list(compress(f['Phi60s1'][()], constFilter & tr & hasValue)))
+			if 'Unit' in f['Phi60s1'].attrs.keys():
+				unitStr = f['Phi60s1'].attrs['Unit']
+			else:
+				unitStr = '?'
+			axs[0].boxplot(dHourlyPhi60s1, positions=hours)
+			axs[0].set_ylim([0, 1])
+			axs[0].set_ylabel(r'Sig1 $\sigma_\phi$ (' + unitStr + ')')
+			axs[0].set_title('Phase Scintillation, ' + recCode + ', ' + constStr + ', ' + dateStr)
+			axs[0].grid()
+			plotWasMade = True
+
+	if 'Phi60s2' in f.keys():
+		hasValue = f['Phi60s2'][()] > 0
+		if sum(constFilter & hasValue) > 0:
+			dHourlyPhi60s2 = []
+			for hour in hours:
+				tr = [x.hour == hour for x in dt_utcdatetimes]
+				dHourlyPhi60s2.append(list(compress(f['Phi60s2'][()], constFilter & tr & hasValue)))
+			if 'Unit' in f['Phi60s2'].attrs.keys():
+				unitStr = f['Phi60s2'].attrs['Unit']
+			else:
+				unitStr = '?'
+			axs[1].boxplot(dHourlyPhi60s2, positions=hours)
+			axs[1].set_ylim([0, 1])
+			axs[1].set_ylabel(r'Sig2 $\sigma_\phi$ (' + unitStr + ')')
+			axs[1].set_xlabel('UTC hour-of-day')
+			axs[1].grid()
+			plotWasMade = True
+
+	if plotWasMade:
+		plotfname = datapath + '/' + fnamestem + '_PhaseScintBoxPlot' + '_' + constStr + '.png'
+		plt.savefig(plotfname, dpi=300)
+		print("Saved plot to " + plotfname)
+
+
+def plot_sky_sigPhi(constFilter, constStr, sigNum):
+	datatype = 'Phi60s' + str(sigNum)
+	if datatype in f.keys():
+		hasValue = f[datatype][()] > 0
+
+		if sum(constFilter & hasValue) == 0:
+			return
+
+		if 'Unit' in f[datatype].attrs.keys():
+			unitStr = f[datatype].attrs['Unit']
+		else:
+			unitStr = '?'
+
+		theta = list(compress(f['Azimuth'][()] * np.pi / 180, constFilter & hasValue))
+		r = list(compress(90 - f['Elevation'][()], constFilter & hasValue))
+		colors = list(compress(f[datatype][()], constFilter & hasValue))
+		area = list(compress(1 + f[datatype][()] * 30, constFilter & hasValue))
+
+		fig = plt.figure()
+		ax = fig.add_subplot(projection='polar')
+		ax.set_theta_zero_location("N")
+		im = ax.scatter(theta, r, c=colors, s=area, cmap=plt.cm.jet, alpha=0.75)
+		im.set_clim([0, 1])
+		ax.set_rlim([0, 90])
+		ax.set_yticks(range(0, 91, 30))
+		ax.set_yticklabels(['90$^{\circ}$', '60$^{\circ}$', '30$^{\circ}$', '0$^{\circ}$'])
+		ax.set_title(datatype + ', ' + recCode + ', ' + constStr + ', ' + dateStr)
+		cb = fig.colorbar(im, ax=ax)
+		cb.set_label(r'Sig' + str(sigNum) + ' $\sigma_\phi$ (' + unitStr + ')')
+
+		plotfname = datapath + '/' + fnamestem + '_' + 'Phi60s' + str(sigNum) + '_' + 'SkyPlot' + '_' + constStr + '.png'
+		plt.savefig(plotfname, dpi=300)
+		print("Saved plot to " + plotfname)
 
 
 
@@ -64,8 +145,9 @@ parser.add_argument("--GLONASS", action="store_true", help="Make plots for GLONA
 parser.add_argument("--Galileo", action="store_true", help="Make plots for Galileo")
 parser.add_argument("--BeiDou", action="store_true", help="Make plots for BeiDou")
 parser.add_argument("--SBAS", action="store_true", help="Make plots for SBAS")
-parser.add_argument("--plot_ts_simple", action="store_true", help="Plot scintillation indices as simple time series")
-parser.add_argument("--plot_ts_box", action="store_true", help="Plot scintillation indices as box-and-whiskers time series")
+parser.add_argument("--plot_ts_simple", action="store_true", help="Plot simple time series of scintillation indices as simple time series")
+parser.add_argument("--plot_ts_box", action="store_true", help="Plot box-and-whiskers time series of scintillation indices")
+parser.add_argument("--plot_sky_sigPhi", action="store_true", help="Plot skyplots of phase scintillation indices")
 parser.add_argument("filename", help="Filename of input data file")
 args = parser.parse_args()
 config = vars(args)
@@ -97,6 +179,7 @@ isGalileo = (f['SVID'][()] >  70) & (f['SVID'][()] < 107)
 isSBAS    = ((f['SVID'][()] > 119) & (f['SVID'][()] < 141)) | ((f['SVID'][()] > 197) & (f['SVID'][()] < 216))
 isBeiDou  = ((f['SVID'][()] > 140) & (f['SVID'][()] < 181)) | ((f['SVID'][()] > 222) & (f['SVID'][()] < 246))
 
+
 # Make plot - Simple scatter plot time series
 if(config['plot_ts_simple']):
 	if(config['GPS']):
@@ -111,81 +194,38 @@ if(config['plot_ts_simple']):
 		plot_timeseries_simple(isSBAS, "SBAS")
 
 
-
 # Make plot - Box-and-whiskers time series
-
-hours = range(0, 24)
-fig, axs = plt.subplots(2)
-plotWasMade = False
-if 'Phi60s1' in f.keys():
-	hasValue = f['Phi60s1'][()] > 0
-	dHourlyPhi60s1 = []
-	for hour in hours:
-		tr = [x.hour == hour for x in dt_utcdatetimes]
-		dHourlyPhi60s1.append(list(compress(f['Phi60s1'][()], isGPS & tr & hasValue)))
-	if 'Unit' in f['Phi60s1'].attrs.keys():
-		unitStr = f['Phi60s1'].attrs['Unit']
-	else:
-		unitStr = '?'
-	axs[0].boxplot(dHourlyPhi60s1, positions=hours)
-	axs[0].set_ylim([0, 1])
-	axs[0].set_ylabel(r'Sig1 $\sigma_\phi$ (' + unitStr + ')')
-	axs[0].set_title('Phase Scintillation, ' + recCode + ', GPS, ' + dateStr)
-	axs[0].grid()
-	plotWasMade = True
-
-if 'Phi60s2' in f.keys():
-	hasValue = f['Phi60s2'][()] > 0
-	dHourlyPhi60s2 = []
-	for hour in hours:
-		tr = [x.hour == hour for x in dt_utcdatetimes]
-		dHourlyPhi60s2.append(list(compress(f['Phi60s2'][()], isGPS & tr & hasValue)))
-	if 'Unit' in f['Phi60s2'].attrs.keys():
-		unitStr = f['Phi60s2'].attrs['Unit']
-	else:
-		unitStr = '?'
-	axs[1].boxplot(dHourlyPhi60s2, positions=hours)
-	axs[1].set_ylim([0, 1])
-	axs[1].set_ylabel(r'Sig2 $\sigma_\phi$ (' + unitStr + ')')
-	axs[1].set_xlabel('UTC hour-of-day')
-	axs[1].grid()
-	plotWasMade = True
-
-if plotWasMade:
-	plt.savefig(datapath + '/' + fnamestem + '_PhaseScintBoxPlot' + '_GPS' + '.png', dpi=300)
+if(config['plot_ts_box']):
+	if(config['GPS']):
+		plot_timeseries_box(isGPS, "GPS")
+	if(config['GLONASS']):
+		plot_timeseries_box(isGLONASS, "GLONASS")
+	if(config['Galileo']):
+		plot_timeseries_box(isGalileo, "Galileo")
+	if(config['BeiDou']):
+		plot_timeseries_box(isBeiDou, "BeiDou")
+	if(config['SBAS']):
+		plot_timeseries_box(isSBAS, "SBAS")
 
 
+# Make plot - Skyplot
 
-# Make plot - Skyplot (TODO)
-
-if 'Phi60s1' in f.keys():
-	hasValue = f['Phi60s1'][()] > 0
-
-	theta = list(compress(f['Azimuth'][()] * np.pi / 180, isGPS & hasValue))
-	r = list(compress(90 - f['Elevation'][()], isGPS & hasValue))
-	colors = list(compress(f['Phi60s1'][()], isGPS & hasValue))
-	area = list(compress(1 + f['Phi60s1'][()] * 30, isGPS & hasValue))
-
-	fig = plt.figure()
-	ax = fig.add_subplot(projection='polar')
-	ax.set_theta_zero_location("N")
-	im = ax.scatter(theta, r, c=colors, s=area, cmap=plt.cm.jet, alpha=0.75)
-	im.set_clim([0, 1])
-	ax.set_rlim([0, 90])
-	ax.set_yticks(range(0, 91, 30))
-	ax.set_yticklabels(['90$^{\circ}$', '60$^{\circ}$', '30$^{\circ}$', '0$^{\circ}$'])
-	ax.set_title('Phase Scintillation, ' + recCode + ', GPS, ' + dateStr)
-	cb = fig.colorbar(im, ax=ax)
-	cb.set_label(r'Sig1 $\sigma_\phi$ (' + unitStr + ')')
-
-	plt.savefig(datapath + '/' + fnamestem + '_' + 'Phi60s1' + '_' + 'SkyPlot' + '_GPS' + '.png', dpi=300)
+if(config['plot_sky_sigPhi']):
+	if(config['GPS']):
+		plot_sky_sigPhi(isGPS, "GPS", 1)
+	if(config['GLONASS']):
+		plot_sky_sigPhi(isGLONASS, "GLONASS", 1)
+	if(config['Galileo']):
+		plot_sky_sigPhi(isGalileo, "Galileo", 1)
+	if(config['BeiDou']):
+		plot_sky_sigPhi(isBeiDou, "BeiDou", 1)
+	if(config['SBAS']):
+		plot_sky_sigPhi(isSBAS, "SBAS", 1)
 
 
 # Make plot - Dots on map (TODO)
 # 'Longitude', 'Latitude'
 
-
-# TODO - Other constellations
 
 # TODO - CN0 plots
 # 'AvgCN0s1', 'AvgCN0s2', 'AvgCN0s3'
